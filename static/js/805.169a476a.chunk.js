15911cf390ae41209d47316c16c0f0cce269df12
vmaletskaya <114164187+vmaletskaya@users.noreply.github.com>
2023-05-30 12:15:36 +0000

    Deploying to gh-pages from @ vmaletskaya/goit-react-hw-05-movies@ebb3c987d793f5775f4b925142e2b872fa70d5bc 🚀

---
+++ static/js/805.169a476a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,r){r.r(t);var n=r(87),o=r(184);t.default=function(){return(0,o.jsxs)("div",{children:[(0,o.jsx)("h1",{children:"Page not found"}),(0,o.jsx)("p",{children:"Do you want return to homepage?"}),(0,o.jsx)(n.rU,{to:"/",children:"Go Home"})]})}}}]);
+//# sourceMappingURL=805.169a476a.chunk.js.map
