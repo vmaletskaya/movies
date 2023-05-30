8dec5115386497083614aa1f9bf4659ae4a13def
Vika Maletskaya <v.maletskayaah@gmail.com>
2023-05-30 15:20:31 +0300

    format

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import  App from 'components/App/App';
+import App from 'components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
     </BrowserRouter>
   </React.StrictMode>
